a1d61e9e67a2c0b163b64b31da465f4a691af5e8
Rishi2553 <rishipanneerselvam@mail.com>
2026-06-13 00:52:19 +0530

    created Portfolio.jsx

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,14 @@
+function Portfolio() {
+  return (
+    <div className="project-card">
+        <h3>Project 1</h3>
+        <p className="text">
+            This was my first portfolio website, built to present my projects,
+             skills, and experience in a professional way. It served as an important
+              step in improving my React, CSS, and UI design skills.
+        </p>
+    </div>
+  );
+}
+
+export default Portfolio;
