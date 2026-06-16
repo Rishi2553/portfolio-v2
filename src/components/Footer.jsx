7cad1d8c21f4754276798505255fd38b4181b7d0
Rishi2553 <rishipanneerselvam@mail.com>
2026-06-16 22:41:41 +0530

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer className="sidebar-footer">
+            <p>© 2026 Rishi Panneerselvam</p>
+            <span>All Rights Reserved</span>
+        </footer>
+    );
+}
+
+export default Footer;
