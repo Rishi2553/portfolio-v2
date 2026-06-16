f6caaf836bee19481d4edbc58affe2e32b0709bd
Rishi2553 <rishipanneerselvam@mail.com>
2026-06-16 22:41:10 +0530

    Add profile section component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import ProfilePicture from "../assets/profile.png";
+import SocialLinks from "./SocialLinks";
+import "./Profile.css";
+
+function Profile() {
+  return (
+    <div className="profile-section">
+
+      <div className="profile-image-container">
+        <img
+          src={ProfilePicture}
+          alt="Rishi Panneerselvam"
+          className="profile-image"
+        />
+      </div>
+
+      <h3 className="profile-name">
+        Rishi Panneerselvam
+      </h3>
+
+      <p className="profile-role">
+        Full Stack Developer
+      </p>
+
+      <SocialLinks />
+
+    </div>
+  );
+}
+
+export default Profile;
