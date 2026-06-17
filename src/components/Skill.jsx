e0b7e174c2fa24fbb4fae7b891e59b279b048566
Rishi2553 <rishipanneerselvam@mail.com>
2026-06-18 02:07:35 +0530

    feat: add staggered animation to skill cards

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,40 +3,65 @@ import SpringIcon from '../assets/icons/spring-boot.png';
 import ReactIcon from '../assets/icons/react.svg';
 import FlutterIcon from '../assets/icons/flutter.png';
 import MySqlIcon from '../assets/icons/mysql.png';
+import {motion} from 'framer-motion';
 // import HtmlIcon from '../assets/icons/html.png';
 // import CssIcon from '../assets/icons/css.png';
 // import JsIcon from '../assets/icons/javascript.png';
 import './Skill.css';
 function Skill() {
+  const container = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: .15
+    }
+  }
+};
+
+const card = {
+  hidden: {
+    opacity: 0,
+    y: 40
+  },
+  show: {
+    opacity: 1,
+    y: 0
+  }
+};
   return (
     <div className="skills-section">
       <h2 className="section-heading">Skills</h2>
 
-      <div className="skills-grid">
-        <div className="skill-card">
+      <motion.div className="skills-grid"
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        <motion.div className="skill-card" variants={card}>
           <img src={javaIcon} alt="Java" />
           <p>Java</p>
-        </div>
+        </motion.div>
 
-        <div className="skill-card">
+        <motion.div className="skill-card" variants={card}>
           <img src={SpringIcon} alt="Spring Boot" />
           <p>Spring Boot</p>
-        </div>
-        <div className="skill-card">
+        </motion.div>
+        <motion.div className="skill-card" variants={card}>
           <img src={ReactIcon} alt="React" />
           <p>React</p>
-        </div>
+        </motion.div>
 
 
-        <div className="skill-card">
+        <motion.div className="skill-card" variants={card}>
           <img src={FlutterIcon} alt="Flutter" />
           <p>Flutter</p>
-        </div>
+        </motion.div>
 
-        <div className="skill-card">
+        <motion.div className="skill-card" variants={card}>
           <img src={MySqlIcon} alt="MySQL" />
           <p>MySQL</p>
-        </div>
+        </motion.div>
 
         {/* <div className="skill-card">
       <img src={HtmlIcon} alt="HTML" />
@@ -52,7 +77,7 @@ function Skill() {
       <img src={JavascriptIcon} alt="JavaScript" />
       <p>JavaScript</p>
     </div> */}
-      </div>
+      </motion.div>
     </div>
   );
 }
