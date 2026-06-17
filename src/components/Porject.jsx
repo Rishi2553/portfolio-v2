46033b3aeaf8aaa458974a1195bd25b915fd3661
Rishi2553 <rishipanneerselvam@mail.com>
2026-06-18 02:07:52 +0530

    feat: add alternating animations to project cards

--- src/components/Porject.jsx
+++ src/components/Porject.jsx
@@ -1,12 +1,27 @@
 import BusTrackPlus from './BusTrackPlus';
 import Portfolio from './Portfolio';
+import {motion} from 'framer-motion';
 import './Project.css';
 function Project() {
   return (
     <div className="project-section">
       <h2 className="section-heading">Projects</h2>
-      <BusTrackPlus />
-      <Portfolio />
+      <motion.div
+        initial={{ opacity:0, x:-100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+  transition={{ duration: 0.8 }}
+  viewport={{ once: true }}
+      >
+        <BusTrackPlus />
+      </motion.div>
+      <motion.div
+        initial={{ opacity:0, x:100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+  transition={{ duration: 0.8 }}
+  viewport={{ once: true }}
+      >
+        <Portfolio />
+      </motion.div>
     </div>
   );
 }
