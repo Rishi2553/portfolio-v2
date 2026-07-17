a6227206d90891eab5e1a96e763617f27a566956
Rishi2553 <rishipanneerselvam@mail.com>
2026-07-18 00:53:33 +0530

    Improve project card navigation and interaction

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./ProjectCard.css";
 
 function ProjectCard({ project }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="project-card">
+    <div className="project-card"
+    onClick={() => navigate(project.route)}>
 
       <div className="project-image">
         <img
@@ -33,7 +36,7 @@ function ProjectCard({ project }) {
             to={project.route}
             className="project-btn"
           >
-            View Details →
+            Explore Project →
           </Link>
         </div>
 
